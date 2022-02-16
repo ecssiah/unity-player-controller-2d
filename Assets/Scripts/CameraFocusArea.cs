642055124e12e5bec952da199d549e17bca00415
ecssiah <michael.benjamin.chapman@gmail.com>
2022-02-16 17:43:18 -0600

    introduce min max to CameraFollow

--- Assets/Scripts/CameraFocusArea.cs
+++ Assets/Scripts/CameraFocusArea.cs
@@ -7,19 +7,17 @@ public class FocusArea
 	public Vector2 Center;
 	public Vector2 Velocity;
 
-	float top, bottom;
-	float left, right;
+	private Vector2 min;
+	private Vector2 max;
 
 	public FocusArea(RectShape _targetRectShape, Vector2 size)
 	{
 		targetRectShape = _targetRectShape;
+		
+		min = new Vector2(targetRectShape.Center.x - size.x / 2, targetRectShape.Center.y - size.y / 2);
+		max = new Vector2(targetRectShape.Center.x + size.x / 2, targetRectShape.Center.y + size.y / 2);
 
-		left = targetRectShape.Center.x - size.x / 2;
-		right = targetRectShape.Center.x + size.x / 2;
-		bottom = targetRectShape.Center.y - size.y / 2;
-		top = targetRectShape.Center.y + size.y / 2;
-
-		Center = new Vector2((left + right) / 2, (top + bottom) / 2);
+		Center = (min + max) / 2;
 		Velocity = Vector2.zero;
 	}
 
@@ -27,29 +25,27 @@ public class FocusArea
 	{
 		Velocity = Vector2.zero;
 
-		if (targetRectShape.Min.x < left)
+		if (targetRectShape.Min.x < min.x)
 		{
-			Velocity.x = targetRectShape.Min.x - left;
+			Velocity.x = targetRectShape.Min.x - min.x;
 		}
-		else if (targetRectShape.Max.x > right)
+		else if (targetRectShape.Max.x > max.x)
 		{
-			Velocity.x = targetRectShape.Max.x - right;
-		}
-
-		if (targetRectShape.Min.y < bottom)
-		{
-			Velocity.y = targetRectShape.Min.y - bottom;
-		}
-		else if (targetRectShape.Max.y > top)
-		{
-			Velocity.y = targetRectShape.Max.y - top;
+			Velocity.x = targetRectShape.Max.x - max.x;
 		}
 
-		left += Velocity.x;
-		right += Velocity.x;
-		top += Velocity.y;
-		bottom += Velocity.y;
+		if (targetRectShape.Min.y < min.y)
+		{
+			Velocity.y = targetRectShape.Min.y - min.y;
+		}
+		else if (targetRectShape.Max.y > max.y)
+		{
+			Velocity.y = targetRectShape.Max.y - max.y;
+		}
 
-		Center = new Vector2((left + right) / 2, (top + bottom) / 2);
+		min += Velocity;
+		max += Velocity;
+
+		Center = (min + max) / 2;
 	}
 }
